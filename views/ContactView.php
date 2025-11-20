adb44936da4d1495583c478f8d8b3a72f3d94cf8
binkingisme <2200002704@nttu.edu.vn>
2025-11-21 06:01:21 +0700

    start

---
+++ views/ContactView.php
@@ -0,0 +1,13 @@
+<?php 
+  //load LayoutTrangTrong.php
+  $this->layoutPath = "LayoutTrangTrong.php";
+ ?>
+<div class="template-customer">
+  <h1>Liên hệ</h1>
+  <iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.13352424751!2d106.73541917591693!3d10.724183189421398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752568547aefbb%3A0xe09007b26ea27534!2zMTEzNS81NS84IEh1eW5oIFRhbiBQaGF0LCBraHUgcGjhu5EgMywgUXXhuq1uIDcsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2sus!4v1763012732109!5m2!1svi!2sus" 
+    width="100%" 
+    height="450" 
+    style="border:0;" 
+    allowfullscreen="" loading="lazy"></iframe>   
+</div>
